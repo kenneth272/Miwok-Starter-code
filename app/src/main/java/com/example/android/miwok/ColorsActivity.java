4c9219c106e1f3c03bbc8a8b98256026efdbe5f0
kenneth272 <kenneth_272@hotmail.com>
2016-12-14 00:45:19 +0800

    lol

--- app/src/main/java/com/example/android/miwok/ColorsActivity.java
+++ app/src/main/java/com/example/android/miwok/ColorsActivity.java
@@ -15,14 +15,14 @@ import java.util.ArrayList;
 
         ArrayList<Word> colors = new ArrayList<>();
 
-        colors.add(new Word("red", "wetetti"));
-        colors.add(new Word("green", "chokokko"));
-        colors.add(new Word("brown", "takaakki"));
-        colors.add(new Word("gray", "topoppi"));
-        colors.add(new Word("black", "kululli"));
-        colors.add(new Word("white", "kelelli"));
-        colors.add(new Word("dusty yellow", "topiisa"));
-        colors.add(new Word("mustard yellow", "chiwiita"));
+        colors.add(new Word("red", "wetetti", R.drawable.color_red));
+        colors.add(new Word("green", "chokokko", R.drawable.color_green));
+        colors.add(new Word("brown", "takaakki", R.drawable.color_brown));
+        colors.add(new Word("gray", "topoppi", R.drawable.color_gray));
+        colors.add(new Word("black", "kululli", R.drawable.color_black));
+        colors.add(new Word("white", "kelelli", R.drawable.color_white));
+        colors.add(new Word("dusty yellow", "topiisa", R.drawable.color_dusty_yellow));
+        colors.add(new Word("mustard yellow", "chiwiita", R.drawable.color_mustard_yellow));
 
         WordAdapter adapter_colors = new WordAdapter(this, colors);
 
--- app/src/main/java/com/example/android/miwok/Word.java
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -4,10 +4,17 @@ public class Word {
 
     private String mDefaultTranslation;
     private String mMiwokTranslation;
+    private int mImageResourceId;
 
     public Word(String defaultTransaltion, String MiwokTranslation) {
         this.mDefaultTranslation = defaultTransaltion;
         this.mMiwokTranslation = MiwokTranslation;
+    }
+
+    public Word(String defaultTransaltion, String MiwokTranslation, int ImageResourceId) {
+        this.mDefaultTranslation = defaultTransaltion;
+        this.mMiwokTranslation = MiwokTranslation;
+        this.mImageResourceId = ImageResourceId;
     }
 
     //Get the default translation of the word
@@ -19,4 +26,6 @@ public class Word {
     public String getMiwokTranslation() {
         return mMiwokTranslation;
     }
+
+    public int getImageResourceId() { return mImageResourceId; }
 }
