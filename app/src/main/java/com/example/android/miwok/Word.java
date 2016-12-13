6f5dc68068e8d69f8014c919235e81459158a968
kenneth272 <kenneth_272@hotmail.com>
2016-12-13 19:16:52 +0800

    Initial commit

---
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -0,0 +1,22 @@
+package com.example.android.miwok;
+
+public class Word {
+
+    private String mDefaultTranslation;
+    private String mMiwokTranslation;
+
+    public Word(String defaultTransaltion, String MiwokTranslation) {
+        this.mDefaultTranslation = defaultTransaltion;
+        this.mMiwokTranslation = mMiwokTranslation;
+    }
+
+    //Get the default translation of the word
+    public String getDefaultTranslation() {
+        return mDefaultTranslation;
+    }
+
+    //Get Miwok Translation of the word
+    public String getMiwokTranslation() {
+        return mMiwokTranslation;
+    }
+}
